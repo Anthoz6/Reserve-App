cccd5c02e2b1473a0c1b473c4d0c9319ec9907ee
Anthoz6 <amestrar21@gmail.com>
2025-07-10 10:28:21 -0500

    CrossOrigin add to AuthController

--- reservapp-backEnd/src/main/java/com/anthonycorp/reservapp/Auth/infrastructure/controller/AuthController.java
+++ reservapp-backEnd/src/main/java/com/anthonycorp/reservapp/Auth/infrastructure/controller/AuthController.java
@@ -5,14 +5,12 @@ import com.anthonycorp.reservapp.Auth.domain.Dto.request.LoginDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     private final LoginUseCase loginUseCase;
